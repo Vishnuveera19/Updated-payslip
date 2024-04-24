1ea287f58a876bafffd6a4cd9993ba3d0a68cd7a
vishnuveeraraghavan1905@gmail.com <vishnuveeraraghavan1905@gmail.com>
2024-04-24 15:45:36 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -28,21 +28,50 @@ import Hrmskillmastertable from './components/hrmskillmaster/Hrmskillmastertable
 import SkillsMasterForm from './components/hrmskillmaster/HrmmSkillsMasterForm';
 import HrmmSpecializationtable from './components/hrmSpecilization/HrmmSpecializationtable';
 import SpecializationForm from './components/hrmSpecilization/HrmmSpecializationForm';
+import LeaveAppForm from './components/LeaveApply/LeaveApplyForm';
+import LeaveApplyTable from './components/LeaveApply/LeaveApplyTable';
+import LeaveSettleForm from './components/LeaveSettlement/LeaveSettlementForm';
+import LeaveSettlementTable from './components/LeaveSettlement/LeaveSettlementTable';
+import LeaveAllocationMasterTable from './components/LeaveAllocationMaster/LeaveAllocationMasterTable';
+import LeaveAllocationForm from './components/LeaveAllocationMaster/LeaveAllocationMasterForm';
+import LeaveApproveHrTable from './components/LeaveApproveHr/LeaveApproveHrTable';
+import ApprovehrForm from './components/LeaveApproveHr/LeaveApproveHrForm';
+import LoanPostTable from './components/LoanPost/LoanPostTable';
+import LoanPostForm from './components/LoanPost/LoanPostForm';
+import LoanPreCloserTable from './components/LoanPreCloser/LoanPreCloserTable';
+import LoanPreclosForm from './components/LoanPreCloser/LoanPreclosureForm';
+import Sample28 from './components/PaymLoan/PaymLoan';
+import PaymLoanTable from './components/PaymLoan/PaymLoanTable';
+import LoanEntryTable from './components/LoanEntry/LoanEntryTable';
+import LoanEntForm from './components/LoanEntry/LoanEntryForm';
+import OnDutyTable from './components/OnDuty/OnDutyTable';
+import OndutyForm from './components/OnDuty/ondutyForm';
+import OtsLabTable from './components/OtsLabs/OtsLabTable';
+import OTSlabForm from './components/OtsLabs/OTSLabForm';
+import Payslip from './components/Payslip';
+import Hello from './components/Payslip';
+import Paycalc from './components/Payslip';
+import { Grid, Button } from '@mui/material';
+import generatePDF from 'react-to-pdf';
+import { useRef } from 'react';
+import ButtonPrint from './components/ButtonPrint'
 
 function App() {
   return (
     <div className="App">
-         {/* <PaymEmployeeForm/> */}
-         {/* <CustomizedTables/> */}
+      <ButtonPrint/>
+         {/* <PaymEmployeeForm/> 
+          <CustomizedTables/> 
          <Router>
           <Routes>
-          {/* <Route path="/" element={<PaymBranchTable />} />
-          <Route path='/paymbranchform' element={<PaymBranchForm/>}/> */}
-          <Route path="/" element={<HrmmSpecializationtable/>}/>
-          <Route path='/HrmmSpecializations' element={<SpecializationForm/>}/>
+           <Route path="/" element={<PaymBranchTable />} />
+          <Route path='/paymbranchform' element={<PaymBranchForm/>}/> 
+          <Route path="/" element={<OtsLabTable/>}/>
+          <Route path='/onduty' element={<OTSlabForm/>}/>
           
           </Routes>
-         </Router>
+         </Router> */}
+         
     </div>
   );
 }
